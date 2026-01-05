a6ed231f3ed811b020018280fec30f9b68579fc1
TongLongDev <tonglong.dev@gmail.com>
2026-01-05 11:54:48 +0700

    rightBar

--- src/components/RightBar.tsx
+++ src/components/RightBar.tsx
@@ -1,21 +1,21 @@
 import Link from "next/link";
-// import PopularTags from "./PopularTags";
-// import Recommendations from "./Recommendations";
-// import Search from "./Search";
+import PopularTags from "./PopularTags";
+import Recommendations from "./Recommendations";
+import Search from "./Search";
 
 const RightBar = () => {
   return (
     <div className="pt-4 flex flex-col gap-4 sticky top-0 h-max">
-      {/* <Search />
+      <Search />
       <PopularTags />
-      <Recommendations /> */}
+      <Recommendations />
       <div className="text-textGray text-sm flex gap-x-4 flex-wrap">
         <Link href="/">Terms of Service</Link>
         <Link href="/">Privacy Policy</Link>
         <Link href="/">Cookie Policy</Link>
         <Link href="/">Accessibility</Link>
         <Link href="/">Ads Info</Link>
-        <span>© 2025 L Corp.</span>
+        <span>© 2026 L Corp.</span>
       </div>
     </div>
   );
